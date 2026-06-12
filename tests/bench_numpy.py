8528ac9cf72801339b063a5e7996ff639a604879
Stephan Hügel <shugel@tcd.ie>
2026-06-12 22:07:15 +0100

    Bump PyO3
    Signed-off-by: Stephan Hügel <shugel@tcd.ie>

--- tests/bench_numpy.py
+++ tests/bench_numpy.py
@@ -1,6 +1,7 @@
 import numpy as np
 import pyperf
 
+
 def add_elems(elems):
     return np.unique(elems)
 
@@ -10,7 +11,5 @@ random_integers = np.random.randint(low=1000000, high=10000000, size=10_000_000)
 
 runner = pyperf.Runner()
 runner.bench_func(
-    "10 million 7-digit random positive integers: numpy",
-    add_elems, random_integers
+    "10 million 7-digit random positive integers: numpy", add_elems, random_integers
 )
-
--- tests/memory_count_distinct.py
+++ tests/memory_count_distinct.py
@@ -1,5 +1,4 @@
 import numpy as np
-import pyperf
 from count_distinct import CVM
 
 
